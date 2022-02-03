52eab11b5d117c872705194f8391483fa73f4749
melonchmp <asamalihamza@gmail.com>
2022-02-03 23:17:31 +0000

    Designed a class for different sound player features.

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
 import pygame
 
-pygame.mixer.init()
+class Player:
+    def __init__(self, song):
+        self.song = song
+        
+        pygame.mixer.init()
+        pygame.mixer.music.load(song)
+        
+    def getstate(self):
+       print(pygame.mixer.music.get_busy())
+    
+    def playsong(self):
+        pygame.mixer.music.play()
+        
+    def pausesong(self):
+        pygame.mixer.music.pause()
+    
+    def unpausesong(self):
+        pygame.mixer.music.unpause()
+    
 
-pygame.mixer.music.load("Music/lofi.mp3")
+pl1 = Player("Music/lofi.mp3",)
+
+
 
 
 while True:
     user_input = input(">> ")
     if user_input == "go":
-        pygame.mixer.music.play()
+        pl1.playsong()
     elif user_input == "p":
-        pygame.mixer.music.pause()
+        pl1.pausesong()
     elif user_input == "up":
-        pygame.mixer.music.unpause()
+        pl1.unpausesong()
+    elif user_input == "state":
+        pl1.getstate()
     elif user_input == "q":
         break
 quit()
