74d8299cac23a2bc6231028832aa416e5e827042
melonchmp <asamalihamza@gmail.com>
2022-02-04 00:31:44 +0000

    Removed the class desing (there is no need), Added multiple sounds playing on different channels at the same time as well as volume control for individual channel sounds

--- main.py
+++ main.py
@@ -1,40 +1,30 @@
 import pygame
 
-class Player:
-    def __init__(self, song):
-        self.song = song
-        
-        pygame.mixer.init()
-        pygame.mixer.music.load(song)
-        
-    def getstate(self):
-       print(pygame.mixer.music.get_busy())
-    
-    def playsong(self):
-        pygame.mixer.music.play()
-        
-    def pausesong(self):
-        pygame.mixer.music.pause()
-    
-    def unpausesong(self):
-        pygame.mixer.music.unpause()
-    
+pygame.mixer.init()
 
-pl1 = Player("Music/lofi.mp3",)
-
-
-
-
+pygame.mixer.Channel(0).play(pygame.mixer.Sound("Music/lofi.mp3"))
+pygame.mixer.Channel(1).play(pygame.mixer.Sound("Music/rain.mp3"))
+vol_list = list()
+def volume_control(x, y):
+    pygame.mixer.Channel(x).set_volume(y)
+    print("Channel {x} volume set to {y}")
 while True:
     user_input = input(">> ")
-    if user_input == "go":
-        pl1.playsong()
-    elif user_input == "p":
-        pl1.pausesong()
+    if user_input == "p":
+        pygame.mixer.Channel(0).pause()
+        pygame.mixer.Channel(1).pause()
     elif user_input == "up":
-        pl1.unpausesong()
+        pygame.mixer.Channel(0).unpause()
+        pygame.mixer.Channel(1).unpause()
     elif user_input == "state":
-        pl1.getstate()
+        pygame.mixer.Channel(0).get_busy()
+        pygame.mixer.Channel(1).get_busy()
+    elif user_input == "volume":
+        ask = input("Enter (channel/volume): ")
+        for i in ask.split("/"):
+            vol_list.append(i)
+        volume_control(int(vol_list[0]), float(vol_list[1]))
+        vol_list.clear()
     elif user_input == "q":
         break
 quit()
