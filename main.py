ac69e721d54b0c54d4decc8bdda0433e7b97ac00
melonchmp <asamalihamza@gmail.com>
2022-02-17 17:05:09 +0000

    Added brand new hd sounds and bound them to buttons that play them and set them to a specified volume.

--- main.py
+++ main.py
@@ -9,8 +9,11 @@ import pygame
 pygame.mixer.pre_init(frequency = 44100, size = -16, channels = 1, buffer = 2**12)
 pygame.mixer.init()
 
-rain = pygame.mixer.Sound('Music/rain.mp3')
-bells = pygame.mixer.Sound('Music/bells.mp3')
+rain = pygame.mixer.Sound('Music/rainthunder.mp3')
+fire = pygame.mixer.Sound('Music/fireplace.mp3')
+rainoncar = pygame.mixer.Sound('Music/rainoncar.mp3')
+seawaves = pygame.mixer.Sound('Music/seawaves.mp3')
+wind = pygame.mixer.Sound('Music/wind.mp3')
 
 class FloatLayout(FloatLayout):
     pass
@@ -24,12 +27,32 @@ class SleepApp(App):
     def rain(self):
         channel_one = pygame.mixer.Channel(0)
         channel_one.play(rain, fade_ms=700)
-        print("Rain Button Pressed")
+        rain.set_volume(0)
+        print("Rain sound started.")
         
-    def bells(self):
+    def fire(self):
         channel_one = pygame.mixer.Channel(1)
-        channel_one.play(bells, fade_ms=700)
-        print("Bells Button Pressed")
+        channel_one.play(fire, fade_ms=700)
+        fire.set_volume(0.3)
+        print("Fire Place sound started.")
+    
+    def rainoncar(self):
+        channel_one = pygame.mixer.Channel(2)
+        channel_one.play(rainoncar, fade_ms=700)
+        rainoncar.set_volume(0)
+        print("Rain on car sound started.")
+
+    def seawaves(self):
+        channel_one = pygame.mixer.Channel(3)
+        channel_one.play(seawaves, fade_ms=700)
+        seawaves.set_volume(0)
+        print("Sea Waves sound started.")
+
+    def wind(self):
+        channel_one = pygame.mixer.Channel(4)
+        channel_one.play(wind, fade_ms=700)
+        wind.set_volume(0)
+        print("Wind sound started.")
 
 
 if __name__ == '__main__':
